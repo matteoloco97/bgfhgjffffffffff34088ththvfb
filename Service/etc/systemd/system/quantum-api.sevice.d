b360c5788109a672455423514c100ee5717e82cd
matteoloco97 <matteoscarfini3@gmail.com>
2025-11-18 12:40:58 +0100

    Delete Service directory

--- Service/etc/systemd/system/quantum-api.sevice.d
+++
@@ -1,6 +0,0 @@
-[Service]
-WorkingDirectory=/root/quantumdev-open
-EnvironmentFile=/root/quantumdev-open/.env
-Environment=CHROMA_PERSIST_DIR=/root/quantumdev-open/storage/chroma
-Environment=EMBEDDING_MODEL_NAME=sentence-transformers/all-MiniLM-L6-v2
-EOF
